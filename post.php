f8128cc0a5099b944621b4a5a336a119b5dfc756
Abhay <ixabhay@gmail.com>
2017-05-26 10:05:18 +0530

    Added hidden field spam protection
    This uses a hidden field defined in index.php [necessary]

--- post.php
+++ post.php
@@ -1,9 +1,14 @@
 <?php
+if(!empty($_POST['humans'])) {
+	                die("Suck it spammers!"); 
+} else {
+	// it's human
 $sContent = $_POST['content'];
-$db = mysqli_connect('localhost','userName','passWord','dataBaseName')
+$db = mysqli_connect('localhost','id1351281_user','yahbas12345','id1351281_post')
  or die('Error connecting to MySQL server.');
 $query = "INSERT INTO `post` (`id`, `content`, `timestamp`) VALUES (NULL, '".$sContent."', CURRENT_TIMESTAMP)";
 mysqli_query($db, $query) or die('Error querying database.');
 mysqli_close($db);
 header('Location: ' . $_SERVER['HTTP_REFERER']);
+}
 ?>
